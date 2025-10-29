35135cc084dfd906e501376e958df5a6a753e30c
simd-float4x4 <asobu.developers+mathew@gmail.com>
2025-10-29 14:41:04 +0900

    fix: Chapter:5まで

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
+import { inter, lustiana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <div className={styles.shape} />
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
@@ -17,6 +21,9 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          <p className={`${lustiana.className} antialiased`}>
+            Now it's your turn! In your fonts.ts file, import a secondary font called Lusitana and pass it to the ｐ element in your /app/page.tsx file. In addition to specifying a subset like you did before, you should also specify different font weights. For example, 400 (normal) and 700 (bold).
+          </p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -26,6 +33,21 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
